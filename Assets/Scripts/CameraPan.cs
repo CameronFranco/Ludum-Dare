d99d28c3d344976dd49a214024a21e293f8a2a1f
Burton <burtonkeck@outlook.com>
2018-12-01 12:47:33 -0500

    MainMenu

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    void Start () {
+        rb = gameObject.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.AddForce(new Vector2(70f, 0.0f));
+    }
+}
