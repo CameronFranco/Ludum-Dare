5c7c11490f9705be6f8cc1d5e9f2db9796260f2e
Burton <burtonkeck@outlook.com>
2018-12-01 17:48:01 -0500

    island selection finish

---
+++ Assets/Scripts/Island3script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Island3script : MonoBehaviour
+{
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
